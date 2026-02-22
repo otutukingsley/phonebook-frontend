842d2ad08af660bf94b0e402f97cbf9bb6b6752e
otutukingsley <chineduotutu3@gmail.com>
2026-02-22 11:13:04 +0100

    adds deployment config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,32 +2,36 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   features: { inlineStyles: true },
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
+    pageTransition: { name: "page", mode: "out-in" },
   },
   runtimeConfig: {
     apiBaseUrl: process.env.NUXT_API_BASE_URL || "",
     session: {
       password: process.env.NUXT_SESSION_PASSWORD || "",
-      maxAge: 60 * 60 * 24 * 7
-    }
+      maxAge: 60 * 60 * 24 * 7,
+    },
   },
   typescript: {
     strict: true,
   },
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-    'nuxt-auth-utils'
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+    "nuxt-auth-utils",
   ],
 
   css: ["~/assets/css/main.css"],
   vite: { plugins: [tailwindcss()] },
-})
+  nitro: {
+    preset: "aws-lambda",
+    serveStatic: false, // S3 will handle static assets
+  },
+});
